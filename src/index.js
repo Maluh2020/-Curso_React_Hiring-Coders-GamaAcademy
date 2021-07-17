1e4a6cf777273711c404858368ac2695e5049ace
Maluh2020 <maluhmyth@gmail.com>
2021-07-17 00:01:46 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Repositories from './Repositories';
+
+ReactDOM.render( 
+    <App />,
+  
+  document.getElementById('root')
+);
+
+
